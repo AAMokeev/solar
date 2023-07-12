83b9cf25d4f04fb63ab5c7891388a59a6a26c243
Artem Mokeev <artem-mokeev@mail.ru>
2023-07-12 12:58:27 +0300

    Add 'solar_station' section

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
 $('.tariff__slider').slick({
     prevArrow: '<img src="images/arrow-prev.svg" alt="" class="tariff__slider-prev-arrow">',
     nextArrow: '<img src="images/arrow-next.svg" alt="" class="tariff__slider-next-arrow">',
+    responsive: [ {
+        breakpoint: 501,
+        settings: {
+          arrows: false,
+          autoplay: true,
+          autoplaySpeed: 2000,
+          vertical: true,
+          dots: true,
+          }
+        }
+      ],
   });
