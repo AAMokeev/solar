39c8156dbb8f63fcfb3275f294ee578bf0f960a9
Artem Mokeev <artem-mokeev@mail.ru>
2023-07-12 10:59:22 +0300

    Add 'tariff' section

---
+++ js/main.js
@@ -0,0 +1,4 @@
+$('.tariff__slider').slick({
+    prevArrow: '<img src="images/arrow-prev.svg" alt="" class="tariff__slider-prev-arrow">',
+    nextArrow: '<img src="images/arrow-next.svg" alt="" class="tariff__slider-next-arrow">',
+  });
